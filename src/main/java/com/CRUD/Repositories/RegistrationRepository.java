2f058602bf70b895371159c6c46e440f7852a842
RahulHavale <rahulhavale256@gmail.com>
2025-04-19 17:14:23 +0530

    Update RegistrationRepository.java

--- src/main/java/com/CRUD/Repositories/RegistrationRepository.java
+++ src/main/java/com/CRUD/Repositories/RegistrationRepository.java
@@ -4,4 +4,5 @@ import com.CRUD.entities.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+  //Code
 }
